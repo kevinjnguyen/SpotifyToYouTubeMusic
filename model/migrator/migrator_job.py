269b01a34cf221c79d1302187250c3ce147560a1
Kevin J Nguyen <kevinjnguyen2@gmail.com>
2024-01-28 10:25:16 -0600

    added builder tests

--- model/migrator/migrator_job.py
+++ model/migrator/migrator_job.py
@@ -1,5 +1,16 @@
+from typing import Dict, List, Self
 from model.spotify import spotify_playlist
 from model.youtube import youtube_playlist
+
+
+class DuplicateFromPlaylistException(Exception):
+    def __init__(self, playlist_id: str):
+        super().__init__(f"an existing job already exists with playlist id: {playlist_id}")
+
+
+class NoSuchJobException(Exception):
+    def __init__(self, playlist_id: str):
+        super().__init__(f"no migration job with id: {playlist_id}")
 
 
 class Job(object):
@@ -15,3 +26,34 @@ class Job(object):
 
     def is_complete(self) -> bool:
         return len(self.from_playlist.tracks) == self.current_track_index
+
+
+class JobsData(object):
+    jobs: Dict[str, Job]
+
+    def __init__(self, jobs: List[Job]):
+        self.jobs = {}
+        for job in jobs:
+            if job.from_playlist.id in jobs:
+                raise DuplicateFromPlaylistException(job.from_playlist.id)
+            else:
+                self.jobs[job.from_playlist.id] = job
+
+    def get_job(self, from_playlist: spotify_playlist.SpotifyPlaylist) -> Job:
+        if from_playlist.id in self.jobs:
+            return self.jobs[from_playlist.id]
+        raise NoSuchJobException(from_playlist.id)
+
+
+class JobsBuilder(object):
+    jobs: List[Job]
+
+    def __init__(self, jobs: List[Job] = []):
+        self.jobs = jobs
+
+    def add_job(self, job: Job) -> Self:
+        self.jobs.append(job)
+        return self
+
+    def build(self) -> JobsData:
+        return JobsData(self.jobs)
--- tests/model/migrator/migrator_job_test.py
+++ tests/model/migrator/migrator_job_test.py
@@ -3,6 +3,8 @@ from model.migrator import migrator_job
 from model.spotify import spotify_playlist
 from model.youtube import youtube_playlist
 
+import pytest
+
 test_spotify_playlist = spotify_playlist.SpotifyPlaylist("from-spotify", "some-spotify-id", "some-spotify-description")
 test_youtube_playlist = youtube_playlist.YouTubePlaylist("to-youtube", "some-youtube-id", "some-youtube-description")
 
@@ -42,3 +44,26 @@ def test_job_is_complete_with_tracks():
     assert new_job.is_complete() == False
     new_job.successful()
     assert new_job.is_complete() == True
+
+
+def test_jobs_data():
+    new_job = migrator_job.Job(test_spotify_playlist, test_youtube_playlist)
+    jobs_data = migrator_job.JobsData([new_job])
+    assert len(jobs_data.jobs) == 1
+    assert jobs_data.jobs[test_spotify_playlist.id] == new_job
+
+    fetched_job = jobs_data.get_job(test_spotify_playlist)
+    assert fetched_job == new_job
+
+    with pytest.raises(Exception) as e_info:
+        new_playlist = spotify_playlist.SpotifyPlaylist("from-spotify", "some-other-id", "some-spotify-description")
+        jobs_data.get_job(new_playlist)
+
+
+def test_jobs_data_builder():
+    jobs_data_builder = migrator_job.JobsBuilder()
+    new_job = migrator_job.Job(test_spotify_playlist, test_youtube_playlist)
+    jobs_data_builder.add_job(new_job)
+    jobs_data = jobs_data_builder.build()
+    assert len(jobs_data.jobs) == 1
+    assert jobs_data.jobs[test_spotify_playlist.id] == new_job
