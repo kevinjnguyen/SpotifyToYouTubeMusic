9bbf9d0a6bdab6d1021fde3dd2301bc42ec10030
Kevin J Nguyen <kevinjnguyen2@gmail.com>
2024-02-10 16:34:47 -0600

    dataclass

--- dao/spotify/spotify_playlist_id.py
+++ dao/spotify/spotify_playlist_id.py
@@ -1,8 +1,7 @@
+from dataclasses import dataclass
+
+
+@dataclass
 class SpotifyPlaylistId:
     def __init__(self, id: str):
         self.id = id
-
-    def __eq__(self, __value: object) -> bool:
-        if isinstance(__value, SpotifyPlaylistId):
-            return self.id == __value.id
-        return True
